54be81ef2d53d6daff9387c6b8b7190eacf350fb
Aosegir <128326345+Aosegir@users.noreply.github.com>
2024-12-29 10:08:44 -0800

    Updated data and allowed for grid view to make locations easier to read

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 //import { useState } from 'react'
 import Data from './Data.jsx';
+import './index.css';
 
 function App() {
   console.log(Data);
   return (
     <>
-      <h1>Hello Odin</h1>
-      <ol>
-        <h2>Item List</h2>
+      <h1>Item List</h1>
+      <div className="location-grid">
         {Data.map((location) => {
           console.log(location);
-          return <div key={location.name}>
+          return <div key={location.name} className="location">
             <h3>{location.name}</h3>
             {location.events.map((event) => {
               return <p key={event.name}>{event.name}</p>
@@ -18,7 +18,7 @@ function App() {
           </div>
           
         })}
-      </ol>
+      </div>
     </>
   )
 }
--- src/Data.jsx
+++ src/Data.jsx
@@ -41,6 +41,90 @@ const Data = [
             },
         ],
     },
+    {
+        name: "Animal Sanctuary",
+        events: [
+            {
+                name: "Pet the animals"
+            },
+            {
+                name: "Volunteer with the rescued animals"
+            },
+            {
+                name: "Cat café"
+            },
+        ],
+    },
+    {
+        name: "Art Exhibition",
+        events: [
+            {
+                name: "Display your own art"
+            },
+            {
+                name: "Interpret the art"
+            },
+            {
+                name: "Attend an artist's q&a"
+            },
+        ],
+    },
+    {
+        name: "Battle Royale",
+        events: [
+            {
+                name: "Make strong alliances"
+            },
+            {
+                name: "Fuckin' hide"
+            },
+            {
+                name: "Go berserk"
+            },
+        ],
+    },
+    {
+        name: "Biker Bar",
+        events: [
+            {
+                name: "Start a brawl"
+            },
+            {
+                name: "Play some pool"
+            },
+            {
+                name: "Grab some drinks"
+            },
+        ],
+    },
+    {
+        name: "Business Summit",
+        events: [
+            {
+                name: "Attend a conference"
+            },
+            {
+                name: "Do some networking"
+            },
+            {
+                name: "Pitch your business ideas to investors"
+            },
+        ],
+    },
+    {
+        name: "Border Of A One-Person Country",
+        events: [
+            {
+                name: "Learn the country's customs"
+            },
+            {
+                name: "Smuggle something out"
+            },
+            {
+                name: "International relations"
+            },
+        ],
+    },
 ];
 
 export default Data;
